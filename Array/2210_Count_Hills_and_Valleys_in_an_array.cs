c4a641d4607954edc9339e8681b406e22dcb92ea
MayurSalvi123 <msalvi660@gmail.com>
2025-07-28 08:18:29 +0530

    2210_Count_Hills_and_Valleys_in_an_array(100-93.55-41.4)

---
+++ Array/2210_Count_Hills_and_Valleys_in_an_array.cs
@@ -0,0 +1,46 @@
+public class Solution {
+    public int CountHillValley(int[] nums) {
+        if (nums.Length < 3)
+        {
+            return 0;
+        }
+        bool ascent = false;
+        var followIndex = 1;
+        var count = 0;
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            if (nums[i] == nums[i+1])
+            {
+                followIndex++;
+                continue;
+            }
+            if (nums[i] < nums[i+1])
+            {
+                ascent = true;
+            }
+            break;
+        }
+
+        for (int i = followIndex + 1; i < nums.Length; i++)
+        {
+            if (nums[i] == nums[followIndex])
+            {
+                followIndex = i;
+                continue;
+            }
+            if (ascent && nums[i] < nums[followIndex])
+            {
+                count++;
+                ascent = false;
+
+            }
+            else if (!ascent && nums[i] > nums[followIndex])
+            {
+                count++;
+                ascent = true;
+            }
+            followIndex = i;
+        }
+        return count;
+    }
+}
