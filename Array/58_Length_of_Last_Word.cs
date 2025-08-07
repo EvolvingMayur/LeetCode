553938012adbcbb65e069b042dadc459e161eb84
MayurSalvi123 <msalvi660@gmail.com>
2025-08-07 22:22:02 +0530

    Added perf stats

--- Array/58_Length_of_Last_Word.cs
+++ Array/58_Length_of_Last_Word.cs
@@ -1,5 +1,6 @@
 public partial class Solution {
     public int LengthOfLastWord(string s) {
+        //100_23.48_40.1
         s = s.TrimEnd();
         var length = 0;
         for (int i = s.Length - 1; i >= 0; i--)
