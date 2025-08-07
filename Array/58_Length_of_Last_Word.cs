1d8261e3d7af865b5ef2b2696497624702e0becd
MayurSalvi123 <msalvi660@gmail.com>
2025-08-07 22:25:58 +0530

    58_Length_Of_Last_Word_100_35.3_40

--- Array/58_Length_of_Last_Word.cs
+++ Array/58_Length_of_Last_Word.cs
@@ -1,9 +1,18 @@
 public partial class Solution {
     public int LengthOfLastWord(string s) {
-        //100_23.48_40.1
-        s = s.TrimEnd();
+        //100_35.3_40
+        var endIndex = s.Length;
+        for (int j = s.Length - 1; j >= 0; j--)
+        {
+            if (s[j] != ' ')
+            {
+                endIndex = j;
+                break;
+            }
+            continue;
+        }
         var length = 0;
-        for (int i = s.Length - 1; i >= 0; i--)
+        for (int i = endIndex; i >= 0; i--)
         {
             if (s[i] == ' ')
             {
@@ -12,5 +21,17 @@ public partial class Solution {
             length++;
         }
         return length;
+        //100_23.48_40.1
+        // s = s.TrimEnd();
+        // var length = 0;
+        // for (int i = s.Length - 1; i >= 0; i--)
+        // {
+        //     if (s[i] == ' ')
+        //     {
+        //         return length;
+        //     }
+        //     length++;
+        // }
+        // return length;
     }
 }
