c40a0105366e354dcbd55a0b4b3b933293b29e26
MayurSalvi123 <msalvi660@gmail.com>
2025-08-07 19:56:11 +0530

    283_Move_Zeroes_6.02_32.79_58.7

---
+++ Array/283_Move_Zeroes.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace LeetCodeSolutions.Array;
+
+public partial class Solution
+{
+    public void MoveZeroes(int[] nums)
+    {
+        int swapRange = nums.Length - 1;
+		for(int i = 0; i< swapRange; i++)
+		{
+			for (int j = i + 1; j <= swapRange; j++)
+			{
+				if (nums[i] == 0 && nums[j] != 0)
+				{
+					(nums[i], nums[j]) = (nums[j], nums[i]);
+					break;
+				}
+			}
+			
+		}
+    }
+}
