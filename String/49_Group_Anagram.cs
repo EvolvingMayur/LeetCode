5b8dd3c0bf4b19419fe727fab4e365123121f50d
MayurSalvi123 <msalvi660@gmail.com>
2025-08-09 01:05:39 +0530

    49_Group_Anagram_61.67_53.6_66.8

--- String/49_Group_Anagram.cs
+++ String/49_Group_Anagram.cs
@@ -1,15 +1,52 @@
 using System;
 
-namespace LeetCodeSolutions.String;
-
-public partial class Solution {
+public partial class Solution
+{
     public IList<IList<string>> GroupAnagrams(string[] strs)
     {
-        if (strs.Length == 1)
+
+        var map = new Dictionary<string, List<string>>();
+
+        foreach (var str in strs)
         {
-            return [[.. strs]];
+            char[] chars = str.ToCharArray();
+            Array.Sort(chars);
+            string key = new string(chars);
+
+            if (!map.TryGetValue(key, out var list))
+            {
+                list = [];
+                map[key] = list;
+            }
+            list.Add(str);
         }
-        return [[.. strs]];
-        
+
+        return [.. map.Values];
+
+        // if (strs.Length == 1)
+        // {
+        //     return [[.. strs]];
+        // }
+        // List<IList<string>> resultList = [[strs[0]]];
+        // for (int i = 1; i < strs.Length; i++)
+        // {
+        //     var added = false;
+        //     for (int j = 0; j < resultList.Count; j++)
+        //     {
+        //         if (IsAnagram(strs[i], resultList[j][0])) // used from Valid Anagram
+        //         {
+        //             added = true;
+        //             resultList[j].Add(strs[i]);
+        //             break;
+        //         }
+        //     }
+        //     if (!added)
+        //     {
+        //         resultList.Add([strs[i]]);
+        //     }
+        // }
+        // return resultList;
+
     }
+    
 }
