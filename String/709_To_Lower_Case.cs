8a8b2d8d7598dd117204a8315e4bc17a1f21864a
MayurSalvi123 <msalvi660@gmail.com>
2025-08-08 22:16:53 +0530

    709_To_Lower_100_74.83_40.2

---
+++ String/709_To_Lower_Case.cs
@@ -0,0 +1,19 @@
+using System.Text;
+
+public partial class Solution {
+    public string ToLowerCase(string s) {
+        StringBuilder result = new StringBuilder();
+        foreach (char c in s)
+        {
+            if(c >= 65 && c <= 90)
+            {
+                result.Append((char)(c + 32));
+            }
+            else
+            {
+                result.Append(c);
+            }
+        }
+        return result.ToString();
+    }
+}
